b254599cacc256dbf4a30b06551e9a5ae4dcb3f6
AnastasiaPetrova1 <petrovanastya.98@mail.ru>
2021-03-19 00:05:14 +0300

    add dialog block

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function Dialog() {
   return(
-    <div>
-      Dialog
+    <div className='dialog'>
+      <div className='message1'>
+		<p className='mes__text1'>Посмотрите, распечатайте, подпишите, потом отсканируйте, пришлите. ОБЯЗАТЕЛЬНО ПЕЧАТАЙТЕ В ЦВЕТЕ!</p>
+	  </div>
+	  <p className='mes__name1'>Любовь Геннадьевна</p>
+	  <div className='message2'>
+		<p className='mes__text2'>Давайте лучше через коллаб</p>
+	  </div>
+	  <p className='mes__name2'>Константин</p>
     </div>
   )
 }
